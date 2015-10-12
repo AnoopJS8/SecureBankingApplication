fd6f9df83df0a9a690d8a196d1619b7d22dc226a
Vageesh Bhasin <vbhasin1@asu.edu>
2015-10-12 16:35:11 -0700

    Added logout

--- src/main/java/com/bankapp/configs/WebSecurityConfig.java
+++ src/main/java/com/bankapp/configs/WebSecurityConfig.java
@@ -43,7 +43,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
-                .permitAll()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
                 .and()
             .sessionManagement()
                  .maximumSessions(1);
