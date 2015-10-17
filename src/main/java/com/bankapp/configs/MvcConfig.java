d7cd0813dbe32cf2027a7cf7070876986b72d73f
Vageesh Bhasin <vbhasin1@asu.edu>
2015-10-16 19:21:04 -0700

    Fixed validation in signup

--- src/main/java/com/bankapp/configs/MvcConfig.java
+++ src/main/java/com/bankapp/configs/MvcConfig.java
@@ -3,12 +3,22 @@ package com.bankapp.configs;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableWebMvc
 @Configuration
 @EnableConfigurationProperties(ThymeleafProperties.class)
 public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/webjars/**")) {
+            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+        }
+    }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
