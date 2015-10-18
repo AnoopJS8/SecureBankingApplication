d4daac7389033c545c07180e11d5553c7fe0448f
Abhishek <aby.tcs@gmail.com>
2015-10-17 19:19:34 -0700

    HTTPS Implementation - Inprogress

--- src/main/java/com/bankapp/configs/WebSecurityConfig.java
+++ src/main/java/com/bankapp/configs/WebSecurityConfig.java
@@ -1,7 +1,13 @@
 package com.bankapp.configs;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.http11.Http11NioProtocol;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -23,39 +29,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers(
-                        "/", 
-                        "/home", 
-                        "/login/identify",
-                        "/login/verifyIdentity",
-                        "/signup",
-                        "/registrationConfirm",
-                        "/resendRegistrationToken",
+        http.authorizeRequests()
+                .antMatchers("/", "/home", "/login/identify",
+                        "/login/verifyIdentity", "/signup",
+                        "/registrationConfirm", "/resendRegistrationToken",
                         "/badUser",
                         // Resources
-                        "/webjars/**"
-                        ).permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .usernameParameter("m_email")
-                .passwordParameter("m_password")
-                .successHandler(new AuthSuccessHandler())
-                .permitAll()
-                .and()
-            .logout()
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login")
-                .and()
-            .sessionManagement()
-                 .maximumSessions(1);
+                        "/webjars/**").permitAll().anyRequest().authenticated()
+                .and().formLogin().loginPage("/login")
+                .usernameParameter("m_email").passwordParameter("m_password")
+                .successHandler(new AuthSuccessHandler()).permitAll().and()
+                .logout().logoutUrl("/logout").logoutSuccessUrl("/login").and()
+                .sessionManagement().maximumSessions(1);
     }
 
     @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(final AuthenticationManagerBuilder auth)
+            throws Exception {
         auth.authenticationProvider(authProvider());
     }
 
@@ -71,4 +61,37 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encoder() {
         return new BCryptPasswordEncoder(12);
     }
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+         
+        // keytool -genkey -alias tomcat -storetype PKCS12 -keyalg RSA -keysize 2048 -keystore keystore.p12 -validity 3650
+        // keytool -list -v -keystore keystore.p12 -storetype pkcs12
+         
+        // curl -u user:password -k https://127.0.0.1:9000/greeting
+         
+        final String keystoreFile = "/keystore.p12";
+        final String keystorePass = "myasubank";
+        final String keystoreType = "PKCS12";
+        final String keystoreProvider = "SunJSSE";
+        final String keystoreAlias = "group7";
+     
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+        factory.addConnectorCustomizers((TomcatConnectorCustomizer) (Connector con) -> {
+            con.setScheme("https");
+            con.setSecure(true);
+            Http11NioProtocol proto = (Http11NioProtocol) con.getProtocolHandler();
+            proto.setSSLEnabled(true);
+            proto.setKeystoreFile(keystoreFile);
+            proto.setKeystorePass(keystorePass);
+            proto.setKeystoreType(keystoreType);
+            proto.setProperty("keystoreProvider", keystoreProvider);
+            proto.setKeyAlias(keystoreAlias);
+        });
+     
+         
+        return factory;
+    }
+
+    
 }
