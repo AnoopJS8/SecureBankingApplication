3f5df24e280b69b1ae00af6a79c2fd37599fa75f
ndhanpal <ndhanpal@asu.edu>
2015-10-18 19:50:12 -0700

    Done with view by user, view by email, Updated pending transaction as per ravin's suggestion

--- src/main/java/com/bankapp/controllers/SystemManagerController.java
+++ src/main/java/com/bankapp/controllers/SystemManagerController.java
@@ -3,16 +3,12 @@
  */
 package com.bankapp.controllers;
 
-import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -20,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.expression.Lists;
 
 import com.bankapp.exceptions.EmailDoesNotExist;
 import com.bankapp.exceptions.EmailExistsException;
@@ -31,7 +26,6 @@ import com.bankapp.models.Account;
 import com.bankapp.models.Transaction;
 import com.bankapp.models.User;
 import com.bankapp.services.ISystemManagerService;
-import com.bankapp.services.IUserService;
 import com.bankapp.constants.Constants;;;
 
 /**
@@ -39,141 +33,149 @@ import com.bankapp.constants.Constants;;;
  *
  */
 @Controller
-public class SystemManagerController implements Constants{
-	
-	
-	
-	
-	@Autowired
-	private ISystemManagerService manager;
-	
+public class SystemManagerController implements Constants {
+
+    @Autowired
+    private ISystemManagerService manager;
+
     private final Logger LOGGER = Logger.getLogger(SystemManagerController.class);
 
-	
-	@RequestMapping(value = "/criticaltransaction", method = RequestMethod.GET)
-	public ModelAndView getPendingTransaction() {
-		List<Transaction> transactions = manager.getTransactionsByStatus(S_OTP_VERIFIED);
-		ModelAndView mv = new ModelAndView();		
-		mv.addObject("critical", transactions);
-		mv.setViewName("manager/criticaltransactions");
-		return mv;
-	}
-	
-	
-	@RequestMapping(value = "/approvetransaction", method = RequestMethod.POST)	   
-	   public ModelAndView approvetransaction(@ModelAttribute("row") Transaction Id, BindingResult result,WebRequest request, Errors errors, Principal principal) {
-	      ModelAndView mv = new ModelAndView();	
-	      System.out.println("Entered Approve" );
-		  System.out.println("Transaction" + Id.getTransactionId());
-			
-		  Transaction transaction = manager.getTransactionbyid(Id.getTransactionId());
-		    
-		  Account FromAccount = transaction.getFromAccount();
-		  Account ToAccount = transaction.getToAccount();
-	      Double AmountToBeSent = transaction.getAmount();
-		  System.out.println(AmountToBeSent);
-		
-		  Double FromAccountBalance = FromAccount.getBalance();
-		  System.out.println(FromAccountBalance);
+    @RequestMapping(value = "/criticaltransaction", method = RequestMethod.GET)
+    public ModelAndView getCriticalTransaction() {
+        List<Transaction> transactions = manager.getTransactionsByStatus(S_OTP_VERIFIED);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("critical", transactions);
+        mv.setViewName("/manager/viewTransaction");
+        return mv;
+    }
+    
+    @RequestMapping(value = "/pendingtransaction", method = RequestMethod.GET)
+    public ModelAndView getInitiatedTransaction() {
+        List<Transaction> transactions = manager.getTransactionsByStatus(S_PENDING);
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("pending", transactions);
+        mv.setViewName("/manager/viewPending");
+        return mv;
+    }
+    
+    @RequestMapping(value = "/getUserById", method = RequestMethod.GET)
+    public ModelAndView getuserId(){
+        ModelAndView mv = new ModelAndView();
 
-		  String str = "";
-			
-		  if(FromAccountBalance > AmountToBeSent)
-		  {
-				manager.reflectChangesToSender(FromAccount, FromAccountBalance, AmountToBeSent);
-				Double ToAccountBalance = ToAccount.getBalance();
-				manager.reflectChangesToReceiver(ToAccount, ToAccountBalance, AmountToBeSent);	
-				str = manager.approveTransaction(transaction);
-		  }
-		  else
-		  {
-				str = "Unsuccessfull";
-		  }
-		
-		  mv.addObject("result1", str);
-		  System.out.println("Done");
-		  mv.setViewName("manager/criticaltransactions");
-	  
-	      return mv;
-	   }
-	
-		
-	@RequestMapping(value = "/manager_adduser", method = RequestMethod.POST)
-	public ModelAndView addUser(User user_request){
-		User user = null;
-		
-		try{
-			user = manager.addUser(user_request);
-		}
-		catch (EmailExistsException e) {
-			String message = String.format("Action: %s, Message: %s", "email_exists", e.getMessage());
+    	mv.setViewName("/manager/viewUserByIdForm");
+    	return mv;
+    }
+    @RequestMapping(value = "/getUserByEmail", method = RequestMethod.GET)
+    public ModelAndView getuserEmail(){
+        ModelAndView mv = new ModelAndView();
+
+    	mv.setViewName("/manager/viewUserByEmailForm");
+    	return mv;
+    }
+
+    @RequestMapping(value = "/approvetransaction", method = RequestMethod.POST)
+    public ModelAndView approvetransaction(@ModelAttribute("row") Transaction Id, BindingResult result,
+            WebRequest request, Errors errors, Principal principal) {
+        ModelAndView mv = new ModelAndView();
+        System.out.println("Entered Approve");
+        System.out.println("Transaction" + Id.getTransactionId());
+
+        Transaction transaction = manager.getTransactionbyid(Id.getTransactionId());
+
+        Account FromAccount = transaction.getFromAccount();
+        Account ToAccount = transaction.getToAccount();
+        Double AmountToBeSent = transaction.getAmount();
+        System.out.println(AmountToBeSent);
+
+        Double FromAccountBalance = FromAccount.getBalance();
+        System.out.println(FromAccountBalance);
+
+        String str = "";
+
+        if (FromAccountBalance > AmountToBeSent) {
+            manager.reflectChangesToSender(FromAccount, FromAccountBalance, AmountToBeSent);
+            Double ToAccountBalance = ToAccount.getBalance();
+            manager.reflectChangesToReceiver(ToAccount, ToAccountBalance, AmountToBeSent);
+            str = manager.approveTransaction(transaction);
+        } else {
+            str = "Unsuccessfull";
+        }
+        System.out.println(str);
+        mv.addObject("result1", str);
+        System.out.println("Done");
+        mv.setViewName("/manager/viewTransaction");
+
+        return mv;
+    }
+    
+    
+
+    @RequestMapping(value = "/manager_adduser", method = RequestMethod.POST)
+    public ModelAndView addUser(User user_request) {
+        User user = null;
+
+        try {
+            user = manager.addUser(user_request);
+        } catch (EmailExistsException e) {
+            String message = String.format("Action: %s, Message: %s", "email_exists", e.getMessage());
             LOGGER.error(message);
             return null;
-		}
-		catch (UserNameExistsException e)
-		{
-			String message = String.format("Action: %s, Message: %s", "username_exists", e.getMessage());
-            LOGGER.error(message);
-            return null;			
-		}
-		catch (UserAlreadyExistException e)
-		{
-			String message = String.format("Action: %s, Message: %s", "user_exists", e.getMessage());
+        } catch (UserNameExistsException e) {
+            String message = String.format("Action: %s, Message: %s", "username_exists", e.getMessage());
             LOGGER.error(message);
             return null;
-		}
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("manager_ceateuser", user);
-		mv.setViewName("manager/manager_view");
-		
-		return mv;
-           
-	}
-	
-	
-	@RequestMapping(value = "/manager_viewuser_byemail", method = RequestMethod.GET)
-	 public ModelAndView getuser_byemail(String email){
-		
-		User user = null;
-		
-		try{
-			user = manager.viewUserByEmail("test@test.com");
-		}
-		catch(EmailDoesNotExist e)
-		{
-			String message = String.format("Action: %s, Message: %s", "EmailDoesNotExist", e.getMessage());
-		    LOGGER.error(message);
-		    return null;
-		}
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("viewuser", user);
-		mv.setViewName("manager/manager_view");
-		return mv;
-	}
-	
-	
-	@RequestMapping(value = "/manager_viewuser_byid", method = RequestMethod.GET)
-	 public ModelAndView getuser_byid(Long id){
-		
-		User user = null;
-		
-		try{
-			user = manager.viewUserById(id);
-		}
-		catch(UserIdDoesNotExist e)
-		{
-			String message = String.format("Action: %s, Message: %s", "EmailDoesNotExist", e.getMessage());
-		    LOGGER.error(message);
-		    return null;
-		}
-		
-		ModelAndView mv = new ModelAndView();
-		mv.addObject("viewuser", user);
-		mv.setViewName("manager/manager_view");
-		return mv;
-	}
-	
-	
+        } catch (UserAlreadyExistException e) {
+            String message = String.format("Action: %s, Message: %s", "user_exists", e.getMessage());
+            LOGGER.error(message);
+            return null;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("manager_ceateuser", user);
+        mv.setViewName("/manager/manager_view");
+
+        return mv;
+
+    }
+
+    @RequestMapping(value = "/manager_viewuser_byemail", method = RequestMethod.POST)
+    public ModelAndView getuser_byemail(@ModelAttribute("email") String email, BindingResult result,
+            WebRequest request, Errors errors, Principal principal) {
+
+        User user = null;
+
+        try {
+            user = manager.viewUserByEmail(email);
+        } catch (EmailDoesNotExist e) {
+            String message = String.format("Action: %s, Message: %s", "EmailDoesNotExist", e.getMessage());
+            LOGGER.error(message);
+            return null;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("viewuser", user);
+        mv.setViewName("/manager/viewUser");
+        return mv;
+    }
+
+    @RequestMapping(value = "/manager_viewuser_byid", method = RequestMethod.POST)
+    public ModelAndView getuser_byid(@ModelAttribute("userid") Long Id, BindingResult result,
+            WebRequest request, Errors errors, Principal principal) {
+
+        User user = null;
+
+        try {
+            user = manager.viewUserById((long)Id);
+        } catch (UserIdDoesNotExist e) {
+            String message = String.format("Action: %s, Message: %s", "EmailDoesNotExist", e.getMessage());
+            LOGGER.error(message);
+            return null;
+        }
+
+        ModelAndView mv = new ModelAndView();
+        mv.addObject("viewuser", user);
+        mv.setViewName("/manager/viewUser");
+        return mv;
+    }
+
 }
--- src/main/java/com/bankapp/services/SystemManagerService.java
+++ src/main/java/com/bankapp/services/SystemManagerService.java
@@ -2,106 +2,95 @@ package com.bankapp.services;
 
 import java.util.List;
 
-import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.bankapp.controllers.SignupController;
 import com.bankapp.models.Account;
 import com.bankapp.models.Transaction;
 import com.bankapp.models.User;
 import com.bankapp.repositories.AccountRepository;
 import com.bankapp.repositories.TransactionRepository;
 import com.bankapp.repositories.UserRepository;
+
 @Service
-public class SystemManagerService implements ISystemManagerService{
-	
-	@Autowired
+public class SystemManagerService implements ISystemManagerService {
+
+    @Autowired
     private UserRepository UserRepo;
-	
-	@Autowired
-	private TransactionRepository TransRepo;
-	
-	@Autowired
-	private AccountRepository AccountRepo;
-	
-	
- 
-	@Override
-	public List<Transaction> getTransactionsByStatus(String status) {
-		
-		List<Transaction> list = TransRepo.findByStatus(status);
-		System.out.println(list);
-		return list;
-	}
-	
-	public User viewUserById(Long id)
-	{
-		User user = UserRepo.findById(id);
-		return user;
-	}
-	
-	public User viewUserByEmail(String email)
-	{
-		User user = UserRepo.findByEmail(email);
-		return user;
-	}
-	
-	public User addUser(User user)
-	{	   
-		UserRepo.save(user);
-		return user;
-	}
-	
-	public String approveTransaction(Transaction transaction)
-	{
-		
-		String result = "";
 
-		try {
-		TransRepo.save(transaction);
-		result = "Successull";
-		System.out.println("Done approve");
-		}catch (Exception e)
-		{
-			result = "unsuccessull";
-		}
+    @Autowired
+    private TransactionRepository TransRepo;
 
-		return result;
-	}
+    @Autowired
+    private AccountRepository AccountRepo;
 
-	@Override
-	public Transaction getTransactionbyid(Long id) {
-		Transaction transaction = TransRepo.findOne(id);
-		return transaction;
-	}
+    @Override
+    public List<Transaction> getTransactionsByStatus(String status) {
+
+        List<Transaction> list = TransRepo.findByStatus(status);
+        System.out.println(list);
+        return list;
+    }
+
+    public User viewUserById(Long id) {
+        User user = UserRepo.findById(id);
+        return user;
+    }
+
+    public User viewUserByEmail(String email) {
+        User user = UserRepo.findByEmail(email);
+        return user;
+    }
+
+    public User addUser(User user) {
+        UserRepo.save(user);
+        return user;
+    }
+
+    public String approveTransaction(Transaction transaction) {
+
+        String result = "";
+        transaction.setStatus("Approved");
+
+        try {
+            TransRepo.save(transaction);
+            result = "Successull";
+            System.out.println("Done approve");
+        } catch (Exception e) {
+            result = "unsuccessull";
+        }
+
+        return result;
+    }
+
+    @Override
+    public Transaction getTransactionbyid(Long id) {
+        Transaction transaction = TransRepo.findOne(id);
+        return transaction;
+    }
+
+    public String reflectChangesToSender(Account account, Double balance, Double amount) {
+        try {
+            account.setBalance(balance - amount);
+            AccountRepo.save(account);
+
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return "Success";
+    }
+
+    public String reflectChangesToReceiver(Account account, Double balance, Double amount) {
+        try {
+            account.setBalance(balance + amount);
+            AccountRepo.save(account);
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+
+        return "Success";
+    }
+
 	
-	
-	
-	public String reflectChangesToSender(Account account,Double balance,Double amount)
-	{
-		try {
-		account.setBalance(balance-amount);
-		AccountRepo.save(account);
-
-		}catch (Exception e)
-		{
-			return e.getMessage();
-		}
-		
-		return "Success";
-	}
-	public String reflectChangesToReceiver(Account account,Double balance,Double amount)
-	{
-		try {
-		account.setBalance(balance+amount);
-		AccountRepo.save(account);
-		}catch (Exception e)
-		{
-			return e.getMessage();
-		}
-		
-		return "Success";
-	}
-
 }
