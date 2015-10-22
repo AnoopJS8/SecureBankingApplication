5137b8b3a5594d55e7d8709a99ed1ee40deab0b1
Vamsi Deepak <pdarbha@asu.edu>
2015-10-21 22:28:38 -0700

    Updated client validation for Date

--- src/main/resources/static/js/profile.js
+++ src/main/resources/static/js/profile.js
@@ -11,29 +11,45 @@ $(document).ready(function() {
 			},
 			'dateOfBirth' : {
 				required : true,
-				dateFA: true
+				dateFA: true,
+				maxDate: true
 			},
 			
 		},
 		// Specify the validation error messages
 		messages : {
-			'address' : "Please enter valid address",
+			'address' : "Address field cannot be NULL",
 			'phoneNumber' : {
-			  required : "phoneNumber cannot be null",
+			  required : "phoneNumber cannot be NULL",
 			  digits : "Please enter a valid phone number"
 			},
-			'dateOfBirth' : "Please enter Date of Birth in mm/dd/yyyy format" 
+			'dateOfBirth' : {
+				dateFA : "Please enter Date of Birth in mm/dd/yyyy format",
+				required : "Date of Birth cannot be NULL",
+				maxDate : "Date of Birth cannot be Future Date"
+			}
 		},
 	
 		submitHandler : function(form) {
 			form.submit();
 		}
 	});
-	
-	$.validator.addMethod( "dateFA", function( value, element ) {
-		return this.optional( element ) || /((0?[1-6]\/((3[0-1])|([1-2][0-9])|(0?[1-9])))|((1[0-2]|(0?[7-9]))\/(30|([1-2][0-9])|(0[1-9]))))\/^[1-4]\d{3}$/.test( value );
-	} , $.validator.messages.date );
+		
 });	
+
+
+$.validator.addMethod( "dateFA", function( value, element ) {
+    return this.optional( element ) || /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/.test( value );
+}, $.validator.messages.date );
+
+
+$.validator.addMethod("maxDate", function (value, element) {
+   var now = new Date();
+   var myDate = new Date(value);
+   return this.optional(element) || myDate <= now;
+
+   
+});
 
 
 	
