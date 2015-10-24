00173a3eefa4fa1595cfd9cc95ea3464fcf19525
Vamsi Deepak <pdarbha@asu.edu>
2015-10-23 17:45:28 -0700

    my changes profile

--- src/main/java/com/bankapp/forms/profileForm.java
+++ src/main/java/com/bankapp/forms/profileForm.java
@@ -1,21 +1,48 @@
 package com.bankapp.forms;
 
-import javax.validation.constraints.NotNull;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotBlank;
 
 public class ProfileForm {
 
-    @NotNull
+    @NotBlank
+    String username;
+      
+    @NotBlank
+    String email;
+    
+    @NotBlank
     String address;
 
-    @NotNull
+    @NotBlank
+    @Pattern(regexp = "[0-9]+" , message = "Please enter a valid Phone Number")
+    @Size(min = 10,max =10, message = "The Phone number should be 10 numbers long")
     String phoneNumber;
-
-    @NotNull
-    @DateTimeFormat(pattern = "MM/dd/yyyy")
+    
+    @NotBlank
+    @Pattern(regexp = "(0?[1-9]|1[012])/(0?[1-9]|[12][0-9]|3[01])/((19|20)\\d\\d)", message = "Please enter date in MM/dd/yyyy format")
     String dateOfBirth;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    
     public String getAddress() {
         return address;
     }
--- src/main/resources/static/js/initTrans.js
+++ src/main/resources/static/js/initTrans.js
@@ -2,58 +2,56 @@ $(document).ready(function() {
 	$('#init-form').validate({
 		errorClass : 'error',
 		rules : {
-		'accountId' : {
-			required : true,
-			digits : true
+			'accountId' : {
+				required : true,
+				digits : true
+			},
+			'amount' : {
+				required : true,
+				digits : true
+			},
+			'comment' : {
+				required : false
+			},
+			'transferDate' : {
+				required : true,
+				minDate : true,
+				dateFA : true
+
+			}
 		},
-		'amount' : {
-			required : true,
-			digits : true
+		// Specify the validation error messages
+		messages : {
+			'accountId' : {
+				required : "Recepient Account number cannot be NULL",
+				digits : "Please enter valid recepient account number"
+			},
+			'amount' : {
+				required : "Transfer amount cannot be NULL",
+				digits : "Please enter valid amount"
+			},
+			'transferDate' : {
+				required : "Transfer date cannot be NULL",
+				dateFA : "Please enter Data in MM/dd/yyyy format",
+				minDate : "Please enter future date"
+			}
 		},
-		'comment' : {
-			required : false
-		},
-		'transferDate' : {
-			required : true,
-			dateFA: true,
-			minDate: true
+
+		submitHandler : function(form) {
+			form.submit();
 		}
-	},
-	// Specify the validation error messages
-	messages : {
-		'accountId' : {
-			required: "Recepient Account number cannot be NULL", 
-			digits:	"Please enter valid recepient account number"
-		},
-		'amount' : {
-			required: "Transfer amount cannot be NULL", 
-			digits:	"Please enter valid amount"
-		},
-		'transferDate' : {
-			required: "Transfer date cannot be NULL" , 
-			dateFA:	"Please enter Data in MM/dd/yyyy format",
-			minDate: "Please enter future date"
-		}},
-
-	submitHandler : function(form) {
-		form.submit();
-	}
+	});
 });
-});	
-	
-$.validator.addMethod( "dateFA", function( value, element ) {
-    return this.optional( element ) || /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/.test( value );
-}, $.validator.messages.date );
 
+$.validator.addMethod("dateFA", function(value, element) {
+	return this.optional(element)
+			|| /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/
+					.test(value);
+}, $.validator.messages.date);
 
-$.validator.addMethod("minDate", function (value, element) {
-   var now = new Date();
-   var myDate = new Date(value);
-   return this.optional(element) || myDate > now;
+$.validator.addMethod("minDate", function(value, element) {
+	var now = new Date();
+	var myDate = new Date(value);
+	return this.optional(element) || myDate > now;
 
-   
 });
-	
-	
-	
-	
