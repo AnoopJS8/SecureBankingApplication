1c136c3bf303e99b36f7eaecb997e2e45932d676
AnoopJS8 <anoop.jyoti88@gmail.com>
2015-10-23 22:53:58 -0700

    Small change in profileform

--- src/main/java/com/bankapp/forms/ProfileForm.java
+++ src/main/java/com/bankapp/forms/ProfileForm.java
@@ -6,8 +6,28 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 public class ProfileForm {
 
+    
+    String username;
+    String email;
+    
     @NotNull
     String address;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
     @NotNull
     String phoneNumber;
